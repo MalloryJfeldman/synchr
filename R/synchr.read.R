7db5dbccfcf8ae9b9717cf52597e27d3a8564c93
Mallory Feldman <42987626+MalloryJfeldman@users.noreply.github.com>
2020-04-21 10:07:31 -0400

    Fixed typo in roxygen text

--- R/synchr.read.R
+++ R/synchr.read.R
@@ -2,7 +2,7 @@
 #'@title Custom function to read in data files into a list
 #'@description synchr requires data to be in list format. This function allows the user to 
 #'read in all individual data files from one folder into a list. Data file must be in .csv format and must include a header.
-#'@param For data, enter the file path to your data folder.
+#'@param data, enter the file path to your data folder.
 
 synchr.read <- function(data) {
   #creates a list of names for each file from the folder based on entered filepath
